a95da9f45e3e172c21e1d25582dee3304c98bfc9
sp88wtr <66656520+sp88wtr@users.noreply.github.com>
2020-09-10 14:12:54 +0900

    add files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'mytestlib-sphiarno-88-hello',
+    version = '1.0.0',
+    url = 'http://localhost',
+    author = 'sphiarno-88',
+    author_email = 'test@email.com',
+    description = 'My Test Library',
+    packages = find_packages()
+)
